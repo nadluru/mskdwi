f16bfba49965277fb6b41a99955f7a6e49758759
Nagesh Adluru <adluru@wisc.edu>
2019-03-07 14:13:29 -0600

    basic tractography before ismrm 2019 and for rsna 2019

---
+++ BasicTractography.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+bet data_combi.nii.gz data_combi_bet -m
+image2voxel -4dimage data_combi.nii.gz -inputdatatype float -outputdatatype float -outputfile data_combi.Bfloat
+fsl2scheme -bvalfile bvals -bvecfile bvecs > camino.scheme
+modelfit -inputfile data_combi.Bfloat -inputdatatype float -outputdatatype float -outputfile data_combi_dti.Bfloat -schemefile camino.scheme -model ldt
+echo -e "fa\nmd" | xargs -I %  bash -c 'cat data_combi_dti.Bfloat | % -inputdatatype float -outputdatatype float | voxel2image -inputdatatype float -outputroot % -header data_combi.nii.gz'
+
+track -inputmodel dt -inputfile data_combi_dti.Bfloat -inputdatatype float -outputdatatype float -seedfile data_combi_bet_mask.nii.gz -anisthresh 0 -curvethresh 45 -curveinterval 0.5 -stepsize 0.1 -tracker euler -outputfile TractsLightWeightVis.Bfloat
+cat TractsLightWeightVis.Bfloat | procstreamlines -mintractlength 100 -outputfile TractsLightWeightVisFiltered.Bfloat -seedfile data_combi_bet_mask.nii.gz
+camino_to_trackvis -i TractsLightWeightVisFiltered.Bfloat -o TractsLightWeightVisFiltered.trk --nifti data_combi_bet_mask.nii.gz --phys-coords
+
+cat data_combi_dti.Bfloat | dteig -inputdatatype float -outputdatatype float -inputmodel dt > data_combi_dti_eig.Bfloat
+pdview -inputfile data_combi_dti_eig.Bfloat -inputmodel dteig -header data_combi.nii.gz -inputdatatype float
+
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel --link echo {1} {2} {3} ::: $root/data.nii.gz ::: $root/bvals ::: $root/bvecs
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel -j4 --bar --plus 'bet {} {..}_bet -m;fslmaths {..}_bet_mask.nii.gz -ero {..}_bet_mask_ero.nii.gz;' ::: $root/data.nii.gz
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel -j4 --bar --link ./BasicTractography2.sh {1} {2} {3} {4} ::: $root/data.nii.gz ::: $root/bvals ::: $root/bvecs ::: $root/nodif_brain_mask.nii.gz
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel --link echo {1} {2} {3} {4} ::: $root/data.nii.gz ::: $root/bvals ::: $root/bvecs ::: $root/data_bet_mask_ero.nii.gz
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel -j4 --bar --link ./BasicTractography2.sh {1} {2} {3} {4} ::: $root/data.nii.gz ::: $root/bvals ::: $root/bvecs ::: $root/data_bet_mask_ero.nii.gz
+
+root="DTI_toNagesh_3_1_19/*/??R/DiffusionPreproc/data"
+parallel -j4 --bar 'cat {} | dteig -inputdatatype float -outputdatatype float -inputmodel dt > {.}_eig.Bfloat' ::: $root/data_dti.Bfloat
---
+++ BasicTractography2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export dwi=$1
+bvals=$2
+bvecs=$3
+mask=$4
+
+betvar=${dwi%.nii*}_bet
+dwibfloat=${dwi%.nii*}.Bfloat
+scheme=${dwi%.nii*}_Camino.scheme
+export dti=${dwi%.nii*}_dti.Bfloat
+tracts=${dwi%.nii*}_tracts
+
+####bet $dwi $betvar -m
+# image2voxel -4dimage $dwi -inputdatatype float -outputdatatype float -outputfile $dwibfloat
+# fsl2scheme -bvalfile $bvals -bvecfile $bvecs > $scheme
+modelfit -inputfile $dwibfloat -inputdatatype float -outputdatatype float -outputfile $dti -schemefile $scheme -model ldt -bgmask $mask
+
+parallel -j2 --bar 'cat $dti | {} -inputdatatype float -outputdatatype float | voxel2image -inputdatatype float -outputroot ${dwi%.nii*}_{} -header $dwi' ::: fa md
+
+track -inputmodel dt -inputfile $dti -inputdatatype float -outputdatatype float -seedfile $mask -anisthresh 0 -curvethresh 45 -curveinterval 0.5 -stepsize 0.1 -tracker euler -outputfile $tracts.Bfloat
+cat $tracts.Bfloat | procstreamlines -mintractlength 10 -outputfile ${tracts}Filtered.Bfloat -seedfile $mask
+camino_to_trackvis -i ${tracts}Filtered.Bfloat -o /tmp/tmp${tracts//\//\_}Filtered.trk --nifti $mask --phys-coords
+cp /tmp/tmp${tracts//\//\_}Filtered.trk ${tracts}Filtered.trk
